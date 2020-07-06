d991ec6c1c3a07421fe981963e117e43da12647f
mickaelvy <59137076+mickaelvy@users.noreply.github.com>
2020-07-06 13:17:44 +0200

    Delete SQL

--- WebContent/sql/script categories.sql
+++
@@ -1,4 +0,0 @@
-insert into CATEGORIES values ('Ameublement');
-insert into CATEGORIES values ('Informatique');
-insert into CATEGORIES values ('Sports&Loisirs');
-insert into CATEGORIES values ('Vêtements');
